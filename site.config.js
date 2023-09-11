26c2fb731550b8b80b1941550ae41990d2143170
Joonery <71689512+Joonery@users.noreply.github.com>
2023-09-12 01:34:28 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "joonery",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "LazyBug",
+    bio: "Doing some funny things...",
+    email: "joonery79@gmail.com",
+    linkedin: "None",
+    github: "joonery",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `test blog`,
+      href: "https://github.com/joonery/blog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "test_blog",
+    description: "Test Blogging",
+    theme : "auto",
   },
 
   // CONFIG configration (required)
@@ -47,7 +48,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
